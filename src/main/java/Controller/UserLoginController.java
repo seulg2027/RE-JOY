deeaff7c65137c9642fb2179a8c836de94591174
seulgi2027 <seulgi2027@dgu.ac.kr>
2025-02-06 18:19:11 +0900

    [Remove] gitignore update - context.xml remove

--- src/main/java/Controller/UserLoginController.java
+++ src/main/java/Controller/UserLoginController.java
@@ -15,10 +15,11 @@ public class UserLoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * 
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
