46e28aea6a3030171fff76dd5d8c44597886aeee
Seulg2027 <seulgi2027@dgu.ac.kr>
2025-02-09 00:18:19 +0900

    [Feat] server 실행 시, login redirect

---
+++ src/main/java/Controller/MainController.java
@@ -0,0 +1,18 @@
+package Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class MainController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.sendRedirect("login.jsp");
+    }
+}
